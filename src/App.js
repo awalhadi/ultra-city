3b713253d4c0f9ff598daffd052b4f85fe719180
Md A Awal Hadi <awalhadi5@gmail.com>
2021-02-28 19:06:11 +0600

    country information form an api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Country from './components/Country/Country';
+
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+    .then(res => res.json())
+    .then(data => setCountries(data))
+    .catch(error => console.log(error))
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Noumber of countries: {countries.length}</h1>
+       
       </header>
+       <div className="country-container">
+       {
+         countries.map(country => <Country country={country}></Country>)
+       }
+       </div>
     </div>
   );
 }
---
+++ src/components/Country/Country.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./Country.css";
+
+const Country = (props) => {
+    console.log(props.country);
+    const {name, capital, population, languages, region, flag} = props.country;
+    return (
+        <div className="country">
+            <div className="country-flag">
+                <img src={flag} alt=""/>
+            </div>
+            <div className="country-info">
+                <h3>{name}</h3>
+                <p> Capital: {capital}</p>
+                <p> Languages: {languages[0].name}</p>
+                <p>Population: {population}</p>
+                <p>Region: {region}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Country;
